44c0c9684b8ff228c705134f27e8a41ae824fd18
kusa-mochi <whoatemyapplepie@gmail.com>
2022-06-19 16:12:42 +0900

    replace link to the site.

--- search-rental-manga/pages/index.tsx
+++ search-rental-manga/pages/index.tsx
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://slash-mochi.net/"
           target="_blank"
           rel="noopener noreferrer"
         >
