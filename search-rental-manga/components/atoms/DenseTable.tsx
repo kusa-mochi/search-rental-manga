3895e1cb02d261de16d019e571f221aa2adb55fc
kusa-mochi <whoatemyapplepie@gmail.com>
2022-06-23 20:53:46 +0900

    create DenseTable component.

---
+++ search-rental-manga/components/atoms/DenseTable.tsx
@@ -0,0 +1,39 @@
+import styles from "./DenseTable.module.scss";
+import '../../types/app.d.ts';
+import { Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Paper, Button } from "@mui/material";
+
+type DenseTableInput = {
+    data: MangaItem[];
+};
+
+const DenseTable = (props: DenseTableInput) => {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>タイトル</TableCell>
+                        <TableCell></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.data.map((manga) => (
+                        <TableRow
+                            key={manga.id}
+                            className={styles.bodyRow}
+                        >
+                            <TableCell component="th" scope="row">
+                                {manga.title}
+                            </TableCell>
+                            <TableCell align="center">
+                                <Button href={manga.url}>読む</Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default DenseTable;
