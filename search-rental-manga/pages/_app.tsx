6cbffdc00d5a697717a788eb4323aa5fe9e3036a
kusa-mochi <whoatemyapplepie@gmail.com>
2022-06-25 14:02:32 +0900

    apply default theme.

--- search-rental-manga/pages/_app.tsx
+++ search-rental-manga/pages/_app.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from '@mui/material'
+import theme from '../theme'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
