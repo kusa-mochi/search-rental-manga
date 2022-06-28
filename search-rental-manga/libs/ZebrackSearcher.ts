71483a559545afded7c9159793bd7645f67a2336
kusa-mochi <whoatemyapplepie@gmail.com>
2022-06-28 21:24:27 +0900

    create a searcher for Zebrack.

---
+++ search-rental-manga/libs/ZebrackSearcher.ts
@@ -0,0 +1,29 @@
+import "../types/app.d.ts";
+import { IMangaSearcher } from "./IMangaSearcher";
+import { BasicSearcher } from "./BasicSearcher";
+
+export class ZebrackSearcher implements IMangaSearcher {
+    async Search(query: string, site: SiteSettings): Promise<SearchResult> {
+        const searcher = new BasicSearcher();
+        const result = await searcher.Search(
+            `https://zebrack-comic.shueisha.co.jp/search?keyword=${query}`,
+            '//div[@class="react-swipeable-view-container"]//p[contains(@class, "sc-kDTinF")]',
+            '//div[@class="react-swipeable-view-container"]//img/@src',
+            ''
+        );
+        const newList: MangaItem[] = [];
+        result.mangaList.forEach(mangaItem => {
+            // TODO: urlを書籍ページに飛べるように調整する。
+            const devidedUrl = mangaItem.url.split("/");
+            const mangaId = devidedUrl[5];
+            const newItem: MangaItem = {
+                title: mangaItem.title,
+                id: mangaItem.id,
+                url: `https://zebrack-comic.shueisha.co.jp/title/${mangaId}`
+            }
+            newList.push(newItem);
+        });
+        result.mangaList = newList;
+        return result;
+    }
+}
