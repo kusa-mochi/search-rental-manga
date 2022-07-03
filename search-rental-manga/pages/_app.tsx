11b39e61f6834a8d2151c37f6098ffea376f5c56
kusa-mochi <whoatemyapplepie@gmail.com>
2022-07-03 13:47:51 +0900

    add code for the analytics.

--- search-rental-manga/pages/_app.tsx
+++ search-rental-manga/pages/_app.tsx
@@ -3,11 +3,15 @@ import { StyledEngineProvider } from '@mui/material'
 import theme from '../theme'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
+        <Head>
+          <title>レンタルマンガ横断検索</title>
+        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </StyledEngineProvider>
