9aa18907e72b0c269684e818244d91b6e046eaa4
mochi <whoatemyapplepie@gmail.com>
2023-04-29 23:36:33 +0900

    [WIP]

--- search-rental-manga/libs/PiccomaSearcher.ts
+++ search-rental-manga/libs/PiccomaSearcher.ts
@@ -5,14 +5,20 @@ import { BasicSearcher } from "./BasicSearcher";
 export class PiccomaSearcher implements IMangaSearcher {
     async Search(query: string, site: SiteSettings): Promise<SearchResult> {
         console.log(`start searching @ ${this.constructor.name}`);
-        const searcher = new BasicSearcher();
-        const result = await searcher.Search(
-            `https://piccoma.com/web/search/result?word=${query}`,
-            '//ul[@id="js_infScroll"]//div[@class="PCM-productCoverImage_title"]/span',
-            '//ul[@id="js_infScroll"]//a[contains(@class, "PCM-product js_hoverDescription")]/@href',
-            'https://piccoma.com'
-        );
-        result.siteName = site.title;
+        // const searcher = new BasicSearcher();
+        // const result = await searcher.Search(
+        //     `https://piccoma.com/web/search/result?word=${query}`,
+        //     '//ul[@id="ajax_infScroll"]//div[@class="PCM-productCoverImage_title"]/span',
+        //     '//ul[@id="ajax_infScroll"]//a[contains(@class, "PCM-product js_hoverDescription")]/@href',
+        //     'https://piccoma.com'
+        // );
+        const result = {
+            siteName: site.title,
+            mangaList: [],
+            error: null,
+        };
+        // 総ページ数を取得する。
+        // 総ページ数が4以上の場合、
         console.log(`fin searching @ ${this.constructor.name}`);
         return result;
     }
